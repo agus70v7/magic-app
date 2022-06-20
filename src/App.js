a844582819d951a7ee38a83559d834f7388c1457
agus70v7 <agus70v7@gmail.com>
2022-06-20 20:00:54 -0300

    primer app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://w7.pngwing.com/pngs/647/917/png-transparent-minato-namikaze-rasengan-goku-naruto-uzumaki-goku-blue-sphere-sasuke-uchiha.png"} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rasengan! y Hola mundo para la cabala xD
+          Hola mundo
         </p>
         <a
           className="App-link"
